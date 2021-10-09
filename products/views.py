9f1e285be9e4d24cf9b0f9e8b8ce6737360cae40
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-10-09 15:29:35 +0000

    Add product detail functionality

--- products/views.py
+++ products/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render  # noqa: F401
+from django.shortcuts import render, get_object_or_404  # noqa: F401
 from .models import Product
 
 # Create your views here.
 
 
 def all_products(request):
-    """A view to (a kill) show all products in cluding sorting and search queiries """
+    """A view to (a kill) show all products in cluding sorting and search queiries """    # noqa: E501
 
-    products = Product.objects.all()
+    products = Product.objects.all()  # pylint: disable=maybe-no-member
 
     context = {
         'products': products,
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_details(request, product_id):
+    """A view to (a kill) show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
