5e8dabafb6b2ccedd9c02d45db58d31289e13e9c
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-10-15 16:43:29 +0000

    Add add to bag functionality

--- bag/urls.py
+++ bag/urls.py
@@ -1,7 +1,10 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=unused-import
 from django.contrib import admin  # noqa: F401
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_bag, name='view_bag')
+    path('', views.view_bag, name='view_bag'),
+    path('add/<item_id>', views.add_to_bag, name='add_to_bag'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,5 @@
 # pylint: disable=missing-module-docstring
-from django.shortcuts import render  # noqa: F401
+from django.shortcuts import render, redirect  # noqa: F401
 
 # Create your views here.
 
@@ -8,3 +8,24 @@ def view_bag(request):
     """A view to (a kill) render the bag contents page"""
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add a quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    # checks if the bag variable exists in the session or create it if not
+    bag = request.session.get('bag', {})
+
+    if item_id in bag in list(bag.keys()):
+        # increments quantity if item is already in bag
+        bag[item_id] += quantity
+    else:
+        # adds item to bag
+        bag[item_id] = quantity
+
+    # overwrites session variable with updated version
+    request.session['bag'] = bag
+
+    return redirect(redirect_url)
