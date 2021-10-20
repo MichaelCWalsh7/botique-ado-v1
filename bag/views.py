7f1d040500c1ce5f2da4e8b9d807b455beb67cda
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-10-20 14:13:58 +0000

    Add notification css shopping bag preview and extra messages

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,5 @@
 # pylint: disable=missing-module-docstring
-from django.shortcuts import render, redirect, reverse, HttpResponse  # noqa: F401,E501
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404  # noqa: F401,E501  # pylint: disable=line-too-long
 from django.contrib import messages
 from products.models import Product
 
@@ -15,7 +15,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """Add a quantity of the specified product to the shopping bag"""
 
-    product = Product.objects.get(pk=item_id)  # pylint: disable=no-member
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
@@ -25,28 +25,32 @@ def add_to_bag(request, item_id):
     bag = request.session.get('bag', {})
 
     if size:
+        # pylint: disable=line-too-long
         if item_id in list(bag.keys()):
             # if the item is in the basket, we must check if it the same size,
             # so we can increment
             if size in bag[item_id]['items_by_size']:
                 bag[item_id]['items_by_size'][size] += quantity
+                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')  # noqa: E501
             else:
                 # if not we can simply set the quantity as ordered.
                 bag[item_id]['items_by_size'][size] = quantity
+                messages.success(request, f'Added size {size.upper()} {product.name} to your bag')  # noqa: E501
         else:
             # if the item isn't in the basket, this adds it as a dictionary
             # with the key of items by size. Allows us to have one item ID
             # per item, but still track different sizes. Clever.
             bag[item_id] = {'items_by_size': {size: quantity}}
+            messages.success(request, f'Added six {size.upper()} {product.name} to your bag')  # noqa: E501
     else:
         if item_id in list(bag.keys()):
             # increments quantity if item is already in bag
             bag[item_id] += quantity
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')  # noqa: E501
         else:
             # adds item to bag
             bag[item_id] = quantity
             messages.success(request, f'Added {product.name} to your bag')
-            print(messages)
 
     # overwrites session variable with updated version
     request.session['bag'] = bag
@@ -57,6 +61,8 @@ def add_to_bag(request, item_id):
 def adjust_bag(request, item_id):
     """Adjust the qunatity of specified product by the specified amount"""
 
+    # pylint: disable=line-too-long
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     size = None
     if 'product_size' in request.POST:
@@ -67,15 +73,19 @@ def adjust_bag(request, item_id):
     if size:
         if quantity > 0:
             bag[item_id]['items_by_size'][size] = quantity
+            messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')  # noqa: E501
         else:
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
+                messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')  # noqa: E501
     else:
         if quantity > 0:
             bag[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')  # noqa: E501
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} to your bag')
 
     # overwrites session variable with updated version
     request.session['bag'] = bag
@@ -85,9 +95,10 @@ def adjust_bag(request, item_id):
 
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
-    # pylint: disable=invalid-name,unused-variable,broad-except
+    # pylint: disable=invalid-name,unused-variable,broad-except,line-too-long
 
     try:
+        product = get_object_or_404(Product, pk=item_id)
         size = None
         if 'product_size' in request.POST:
             size = request.POST['product_size']
@@ -98,8 +109,10 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
+                messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')  # noqa: E501
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} to your bag')
 
         # overwrites session variable with updated version
         request.session['bag'] = bag
@@ -107,4 +120,5 @@ def remove_from_bag(request, item_id):
         return HttpResponse(status=200)
 
     except Exception as e:  # noqa: F841
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
