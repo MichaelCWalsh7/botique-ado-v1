8be016345cec5f092b02af23ac8e7ef46dfa3f0c
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-10-31 20:07:16 +0000

    Add cache checkout metadata logic

--- checkout/views.py
+++ checkout/views.py
@@ -1,8 +1,10 @@
 # pylint: disable=missing-module-docstring,missing-function-docstring,line-too-long,unused-variable,unused-import  # noqa: E501
-# pylint: disable=no-member,undefined-variable
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+# pylint: disable=no-member,undefined-variable,broad-except,invalid-name
+import json
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse  # noqa: E501
 from django.contrib import messages
 from django.conf import settings  # noqa: F401
+from django.views.decorators.http import require_POST
 
 import stripe  # noqa: F401
 
@@ -10,6 +12,26 @@ from bag.contexts import bag_contents
 from products.models import Product
 from .forms import OrderForm
 from .models import Order, OrderLineItem
+
+
+@require_POST
+def chache_checkout_date(request):
+    try:
+        # stores payment intent id
+        pid = request.POST.get('client_secret').split('_ secret')[0]
+        # initializes stripe with secret key so we can modify payment intent
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        # call method with pid to modify the payment intent metadata
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
