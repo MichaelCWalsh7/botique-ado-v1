6ad34cf84b2a3403dc74bcc74a82d993ccc28872
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-11-07 20:48:15 +0000

    Add add_product view and form html

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,13 @@
+# pylint: disable=missing-module-docstring,unused-import
 from django.contrib import admin  # noqa: F401
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<product_id>', views.product_details, name='product_detail'),
+    # the int in front of the product id variable ensures that the product
+    # details view won't accept 'add/' as part of a product ID. This occurs
+    # Django always uses the first url it finds a matching pattern for.
+    path('<int:product_id>/', views.product_details, name='product_detail'),
+    path('add/', views.add_product, name='add_product'),
 ]
