b1f9eb519a7515a45f64123ce88dbe5c927cd773
MichaelCWalsh7 <MichaelCWalsh792@gmail.com>
2021-11-10 12:32:02 +0000

    Add delete product functionality

--- products/urls.py
+++ products/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     # Django always uses the first url it finds a matching pattern for.
     path('<int:product_id>/', views.product_details, name='product_detail'),
     path('add/', views.add_product, name='add_product'),
-    path('edit/<int:product_id>', views.edit_product, name='edit_product'),
+    path('edit/<int:product_id>/', views.edit_product, name='edit_product'),
+    path('delete/<int:product_id>/', views.delete_product, name='delete_product'),  # noqa: E501
 ]
